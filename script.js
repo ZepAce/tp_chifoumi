75b969f64ab76ebbdbc7f5f20952813cce99b5fe
Benoit Galet <benoitgalet59460@gmail.com>
2023-03-21 11:48:30 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+// Appel de la fonction de mise en place d'une nouvelle partie
+commencerPartie();
+
+// La suite du script constitue en la définition des fonctions utilisées dans le jeu
+
+/**
+ * Calcule le résultat de la partie, ie. le message de victoire, défaite ou égalité
+ * @param  {Number}   monCoup  coup du joueur
+ * @param  {Number}   coupOrdi  coup de l'ordinateur
+ * @return {String}   result le message correspondant au résultat
+ */
+function calculerResultat(monCoup, coupOrdi) {}
+
+/**
+ * @return {Number}   nombre entier aléatoire entre 0 et 2
+ */
+function coupAleatoire() {
+  return Math.floor(Math.random() * 3);
+}
+
+/**
+ * Mise en place d'une nouvelle partie
+ */
+function commencerPartie() {}
+
+/**
+ * Affiche le résultat final de la partie
+ * @param {Event} event événement contenant les informations de l'entrée utilisateur
+ */
+
+function finirPartie(event) {}
